c628dc854489121c14997056375a7a5ae7c84c7d
sterinaldi <stefano.rinaldi@uni-heidelberg.de>
2024-06-27 14:03:23 +0200

    initial commit

---
+++ DPNF/mixture.py
@@ -0,0 +1,9 @@
+import numpy as np
+from figaro.mixture import DPGMM
+
+class DPNF(DPGMM):
+    """
+    Dirichlet Process Normalising Flow Mixture Model class
+    """
+    def __init__(self):
+        pass
